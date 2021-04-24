e598f2f3bc06f9e99cc65701f26d1d3b22e1f46c
Ezran Bayantemur <ezranbayantemur@gmail.com>
2021-04-24 18:31:40 +0300

    feat: :construction: SECTION: 2 - [Practice]: Cafe List

--- src/App.js
+++ src/App.js
@@ -1,56 +1,78 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   Text,
-  Image,
-  StyleSheet,
+  View,
   FlatList,
-  ScrollView,
-  Dimensions,
+  StyleSheet,
+  Switch,
 } from 'react-native';
-import NewsCard from './components/NewsCard';
 
-import news_data from './news_data.json';
-import news_banner_data from './news_banner_data.json';
+const data = [
+  {id: 0, name: 'cafe.exe', isFavorite: true},
+  {id: 1, name: 'KafaKafe', isFavorite: false},
+  {id: 2, name: 'BugG', isFavorite: false},
+  {id: 3, name: 'Rock`n Coke', isFavorite: true},
+  {id: 4, name: 'do(drink)', isFavorite: false},
+  {id: 5, name: 'esc', isFavorite: false},
+];
 
 function App() {
-  const renderNews = ({item}) => <NewsCard news={item} />;
+  const [cafeList, setCafeList] = useState(data);
+  const [showOnlyFavorites, setShowOnlyFavorites] = useState(false);
+
+  function onFavoritesChange(isFavoriteSelected) {
+    setShowOnlyFavorites(isFavoriteSelected);
+    isFavoriteSelected
+      ? setCafeList(cafeList.filter(cafe => cafe.isFavorite))
+      : setCafeList(data);
+  }
+
+  const renderCafe = ({item}) => {
+    return (
+      <View style={styles.cafe_container}>
+        <Text style={styles.cafe_title}>{item.name}</Text>
+      </View>
+    );
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.headerText}>News</Text>
-      <FlatList
-        ListHeaderComponent={() => (
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            {news_banner_data.map(bannerNews => (
-              <Image
-                style={styles.banner_image}
-                source={{uri: bannerNews.imageUrl}}
-              />
-            ))}
-          </ScrollView>
-        )}
-        keyExtractor={item => item.u_id.toString()}
-        data={news_data}
-        renderItem={renderNews}
-      />
+      <View style={styles.favorite_container}>
+        <Text style={styles.favorite_title}>Show Only Favorites</Text>
+        <Switch value={showOnlyFavorites} onValueChange={onFavoritesChange} />
+      </View>
+      <FlatList data={cafeList} renderItem={renderCafe} />
     </SafeAreaView>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#eceff1',
   },
-  banner_image: {
-    height: Dimensions.get('window').height / 5,
-    width: Dimensions.get('window').width / 2,
+  favorite_container: {
+    justifyContent: 'center',
+    padding: 10,
+    alignItems: 'center',
   },
-  headerText: {
+  favorite_title: {
     fontWeight: 'bold',
-    fontSize: 50,
+    margin: 5,
+  },
+  cafe_container: {
+    margin: 10,
+    borderRadius: 8,
+    borderColor: '#bdbdbd',
+    backgroundColor: '#eceff1',
+    borderWidth: 1,
+    padding: 5,
+  },
+  cafe_title: {
+    fontSize: 25,
+    margin: 10,
+    color: '#263238',
   },
 });
-
-export default App;
